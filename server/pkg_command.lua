57abed889f72af54bd58babbd677d7dd9084e936
alexandregv <contact@alexandregv.fr>
2019-12-19 04:09:26 +0000

    Add "help" subcommand

--- server/pkg_command.lua
+++ server/pkg_command.lua
@@ -9,6 +9,45 @@ local function about(player)
 ]])
 end
 
+local helps_sorted = {
+	[1]  = "help",
+	[2]  = "list",
+	[3]  = "info",
+	[4]  = "start",
+	[5]  = "stop",
+	[6]  = "restart",
+	[7]  = "restart-all",
+	[8]  = "restart-list",
+	[9]  = "set",
+	[10] = "get",
+}
+
+local helps = {
+	["help"] = "/pkg help [cmd] | Print help about commands",
+	["list"] = "/pkg list | List all packages on the server",
+	["info"] = "/pkg info <packages> | Get informations about one or more package(s)",
+	["start"] = "/pkg start <packages> | Start one or more package(s)",
+	["stop"] = "/pkg stop <packages> | Stop one or more package(s)",
+	["restart"] = "/pkg restart <packages> | Restart one or more package(s)",
+	["restart-all"] = "/pkg restart-all | Restart all packages",
+	["restart-list"] = "/pkg restart-list | Restart packages defined with /pkg set restart-list",
+	["set"] = "/pkg set <restart-list|restart-key> | Set restart-list or restart-key value",
+	["get"] = "/pkg get <restart-list|restart-key> | Get restart-list or restart-key value",
+}
+
+local function help(player, cmd)
+	local help = helps[cmd]
+	if cmd == nil then
+		for _, v in ipairs(helps_sorted) do
+			AddPlayerChat(player, "[pkg] "..helps[v])
+		end
+	elseif help then
+		AddPlayerChat(player, "[pkg] "..help)
+	else
+		AddPlayerChat(player, "[pkg] "..helps["help"])
+	end
+end
+
 local cmds = {
 	["about"] = about,
 
@@ -44,10 +83,10 @@ local cmds = {
 AddCommand("pkg", function(player, cmd, ...)
 	local cmdfunc = cmds[cmd]
 	if cmd == nil then
-		cmds["about"](player, {...})
+		cmds["about"](player, ...)
 	elseif cmdfunc then
-		cmdfunc(player, {...})
+		cmdfunc(player, ...)
 	else
-		cmds["help"](player, {...})
+		cmds["help"](player)
 	end
 end )
